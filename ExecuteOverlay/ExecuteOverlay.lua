966cfafc9a63f74b311216c2ad1c7c01994d29f0
vgpunks <4714318+vgpunks@users.noreply.github.com>
2023-10-15 09:23:58 -0400

    Add files via upload

---
+++ ExecuteOverlay/ExecuteOverlay.lua
@@ -0,0 +1,67 @@
+
+-- CONFIGURATION ----------------------------------
+
+
+local hunter = true
+local mage = true
+local paladin = true
+local priest = true
+local rogue = true
+local warlock = true
+local warrior = true
+
+
+-- END OF CONFIG ----------------------------------
+
+
+
+
+local _, class = UnitClass("player")
+
+
+-- Only load the addon for the classes that can, or want, to use it
+if (hunter and (class == "HUNTER")) or (mage and (class == "MAGE")) or (paladin and (class == "PALADIN")) or (priest and (class == "PRIEST")) or (rogue and (class == "ROGUE")) or (warlock and (class == "WARLOCK")) or (warrior and (class == "WARRIOR")) then
+
+	local eventHandler = CreateFrame("frame")
+
+
+	-- Main function
+	local function executeRange(target)
+		local hp = (UnitHealth("target") / UnitHealthMax("target"))
+		local canAttack = UnitCanAttack("player", "target")
+		local dead = UnitIsDead("target")
+		local t = GetActiveSpecGroup()
+		
+		if canAttack then
+			if dead then
+				SpellActivationOverlay_HideOverlays(SpellActivationOverlayFrame, _)
+			elseif (class == "HUNTER" and hp <= 0.20) or (class =="MAGE" and t == 2 and hp <= 0.35) or (class == "PALADIN" and t == 3 and hp <= 0.20) or (class == "PRIEST" and t == 3 and hp <= 0.25) or (class == "ROGUE" and t == 1 and hp <= 0.35) or (class == "WARLOCK" and hp <= 0.25) or (class == "WARRIOR" and hp <= 0.20) then
+				SpellActivationOverlay_ShowOverlay(SpellActivationOverlayFrame, _, 450923, "TOP", 1, 255, 0, 0, false, false)
+		--		SpellActivationOverlay_ShowOverlay(SpellActivationOverlayFrame, _, "TEXTURES\\SPELLACTIVATIONOVERLAYS\\GENERICARC_05.BLP", "LEFT", 0.7, 255, 0, 0, false, false)
+			else
+				SpellActivationOverlay_HideOverlays(SpellActivationOverlayFrame, _)
+			end
+		end
+	end
+
+
+	-- Hide when switching targets
+	local function reset()
+		SpellActivationOverlay_HideOverlays(SpellActivationOverlayFrame, _)
+	end
+
+
+	-- Event handler
+	eventHandler:RegisterEvent("UNIT_HEALTH")
+	eventHandler:RegisterEvent("PLAYER_TARGET_CHANGED")
+
+	eventHandler:SetScript("OnEvent", function(self, event, arg1)
+		if (event == "UNIT_HEALTH") then
+			executeRange()
+		end
+
+		if (event == "PLAYER_TARGET_CHANGED") then
+			reset()
+		end
+	end)
+end
